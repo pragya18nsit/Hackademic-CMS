202721de3bcf103da81c464c4bf4efc884c373d6
pragya18nsit <pragya18nsit@gmail.com>
2012-07-03 20:14:19 +0530

    License

--- model/common/class.Session.php
+++ model/common/class.Session.php
@@ -1,11 +1,33 @@
 <?php
 /**
  *
- * hackademic/model/class.Session.php
+ * Hackademic-CMS/model/common/class.Session.php
  *
- * Session
+ * Hackademic Session Class
+ * The class that manages logged-in Hackademic users' sessions via the web.
+ * 
+ * Copyright (c) 2012 OWASP
  *
- * The object that manages logged-in Hackademic users' sessions via the web.
+ * LICENSE:
+ *
+ * This file is part of Hackademic CMS (https://www.owasp.org/index.php/OWASP_Hackademic_Challenges_Project).
+ *
+ * Hackademic CMS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Hackademic CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with Hackademic CMS.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @author Pragya Gupta <pragya18nsit[at]gmail[dot]com>
+ * @author Konstantinos Papapanagiotou <conpap[at]gmail[dot]com>
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2012 OWASP
  *
  */
 require_once(HACKADEMIC_PATH."/model/common/class.User.php");
